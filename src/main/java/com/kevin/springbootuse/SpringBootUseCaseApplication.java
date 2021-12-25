bef27ecea89e019f3476def34ebd34d49c1968a8
chenjun <kevinchen19888@163.com>
2021-12-25 19:52:23 +0800

    Initial commit

---
+++ src/main/java/com/kevin/springbootuse/SpringBootUseCaseApplication.java
@@ -0,0 +1,13 @@
+package com.kevin.springbootuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUseCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUseCaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kevin/springbootuse/SpringBootUseCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevin.springbootuse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUseCaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
